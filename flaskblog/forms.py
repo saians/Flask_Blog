2e82dd216848f21e3b0d6a9867257c15c41718dc
Saifullah Ansari <saifullah.ansari@gmail.com>
2018-12-20 12:56:28 +0100

    Python Flask Tutorial: Full-Featured Web App Part 6 - User Authentication

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,10 +14,20 @@ class RegistrationForm(FlaskForm):
                              validators=[DataRequired()])
     confirm_password = PasswordField('confirm password',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign up');
+    submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, please choose another one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, please choose another one.')
+
 
 class LoginForm(FlaskForm):
-
     email = StringField('email',
                         validators=[DataRequired(), Email()])
 
